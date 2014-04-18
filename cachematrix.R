ed3c47ba7614ba11124e511662caa297cd5c74f0
Michael Foy <michaelfoy01@hotmail.com>
2014-04-17 23:36:58 -0500

    Foy - Programming Assignment 2
    Added code for the programming assignment 2 as described in the course
    material.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Function to cache various values of the matrix:
+##   1) Set matrix values
+##   2) Get matrix values
+##   3) Set matrix inverse values
+##   4) Get matrix inverse values
 
+makeCacheMatrix <- function(MVal = matrix()) {
+
+     ## Initialize matrix inverse value to NULL
+     MInv <- NULL
+
+     ## Set the value of the matrix to a new value. Since new value,
+     ## initialize the matrix inverse to NULL.
+     setMVal <- function (y) {
+          MVal <<- y
+          MInv <<- NULL
+     }
+
+     ## Return the value of the matrix
+     getMVal <- function() {
+          MVal
+     }
+     
+     ## Set the value of the matrix inverse (using an argument, not computed)
+     setMInv <- function(invValue) {
+          MInv <<- invValue
+     }
+
+     ## Return the value of the matrix inverse
+     getMInv <- function() {
+          MInv
+     }
+     
+     list(setMVal = setMVal, getMVal = getMVal, 
+          setMInv = setMInv, getMInv = getMInv)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+     ## Get the cached value of the inverse
+     s <- x$getMInv()
+     
+     ## If the cached value is not NULL, return that value
+     if (!is.null(s)) {
+          message("Getting cached value of inverse")
+          return(s)
+     }
+     
+     ## If NULL, get the value of the cached matrix
+     data <- x$getMVal()
+     
+     # calculate the inverse of the data matrix
+     s <- solve(data)
+
+     # Set the value of the matrix inverse to the cached matrix
+     x$setMInv(s)
+     
+     # Return the matrix inverse s
+     s
 }
